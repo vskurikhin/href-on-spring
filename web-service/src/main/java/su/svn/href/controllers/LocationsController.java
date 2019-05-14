b4d4d92f28e49feb2206e424f38af3f17521eacc
Victor N. Skurikhin <vskurikhin@gmail.com>
2019-05-14 23:54:26 +0300

    location + x-editable

--- web-service/src/main/java/su/svn/href/controllers/LocationsController.java
+++ web-service/src/main/java/su/svn/href/controllers/LocationsController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import su.svn.href.repository.LocationRepository;
 
 @Controller
@@ -18,8 +21,19 @@ public class LocationsController
     }
 
     @RequestMapping("/locations")
-    public String locations(final Model model)
+    public String locations()
     {
         return "locations";
+    }
+
+    @RequestMapping("/location")
+    public String location(@RequestParam long id, final Model model)
+    {
+//        IReactiveDataDriverContextVariable reactiveDataDrivenMode = new ReactiveDataDriverContextVariable(
+//            locationRepository.findById(id)
+//        );
+        model.addAttribute("location", locationRepository.findById(id));
+
+        return "location";
     }
 }
